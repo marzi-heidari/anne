2b88232e20be598f4474acde89e87a1a834ecbe8
filiperobotic <filipe.rolim@gmail.com>
2024-10-24 08:51:12 -0300

    kmin12

--- main_cifar_ANNE.py
+++ main_cifar_ANNE.py
@@ -51,6 +51,8 @@ parser.add_argument('--warmup', default=0, type=int, metavar='wm', help='number 
 parser.add_argument('--teto', default=200, type=int, metavar='teto', help='teto knn')
 parser.add_argument('--distill_mode', type=str, default='eigen', choices=['kmeans','fine-kmeans','fine-gmm'], help='mode for distillation kmeans or eigen.')
 parser.add_argument('--p_threshold', default=0.5, type=float, help='clean probability threshold')
+parser.add_argument('--kmin1', default=40, type=int, metavar='N', help='kmin1')
+parser.add_argument('--kmin2', default=80, type=int, metavar='N', help='kmin2')
 # parser.add_argument('--operation', type=str, default='inter', choices=['inter','union','union_fine_g1', 'union_fine_g1_g2','union_fine_g3_g4', 'union_fine_g1_g2_g3', 'union_fine_g2_g3_g4', 'sr', 'srbm'], help='mode for selection.')
 
 
