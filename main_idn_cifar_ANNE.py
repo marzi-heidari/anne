fefc802f19612e9b247a1142a60417287fec40d8
filiperobotic <filipe.rolim@gmail.com>
2024-11-26 17:38:39 -0300

    fixed idn bug

--- main_idn_cifar_ANNE.py
+++ main_idn_cifar_ANNE.py
@@ -152,14 +152,14 @@ def evaluate(dataloader, encoder, classifier, args, noisy_label, clean_label, i,
 
         otsu_split = None
         if i>=args.warmup:
-            if  "otsu" in args.radaptive:
+            
                 
-                group_1_clean, group_2_maybe_clean, group_3_maybe_noisy, group_4_noisy = split_dataset(his_score)
-                otsu_split = {'clean_ids':torch.nonzero(group_1_clean),
-                            'maybe_clean_ids': torch.nonzero(group_2_maybe_clean),
-                            'maybe_noisy_ids': torch.nonzero(group_3_maybe_noisy),
-                            'noisy_ids': torch.nonzero(group_4_noisy)
-                }
+            group_1_clean, group_2_maybe_clean, group_3_maybe_noisy, group_4_noisy = split_dataset(his_score)
+            otsu_split = {'clean_ids':torch.nonzero(group_1_clean),
+                        'maybe_clean_ids': torch.nonzero(group_2_maybe_clean),
+                        'maybe_noisy_ids': torch.nonzero(group_3_maybe_noisy),
+                        'noisy_ids': torch.nonzero(group_4_noisy)
+            }
                 
         
         ################################### sample selection ###################################
